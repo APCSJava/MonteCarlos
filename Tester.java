c88022dc2346d8d0bb0ca1eab8c148d39032b25d
Kent Collins <kent.r.collins@gmail.com>
2020-01-24 10:32:37 +0000

    instantiate and test default class instance

--- Tester.java
+++ Tester.java
@@ -2,12 +2,7 @@
 public class Tester {
 
 	public static void main(String[] args) {
-		// MCPi mc = new MCPi(0, 1, 0, 1);
-		// for (int i = 0; i<100000; i++) {
-		// mc.simulate();
-		// }
-		// System.out.println("Results: "+mc.getEstimatedArea()*4);
-		MonteCarlo mc = new MCCubed(0, 1, 0, 1);
+		MonteCarlo mc = new MonteCarlo(0, 1, 0, 1);
 		for (int i = 0; i < 100000; i++) {
 			mc.simulate();
 		}
